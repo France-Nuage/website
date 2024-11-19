35a72751b4a8154d689f69f6bd420fcc409bc18a
Alexandre Cailler <alex02.cailler@gmail.com>
2024-11-19 10:50:06 +0100

    feat: init repo

---
+++ apps/web/pages/index/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <Header />
+    <Comparative />
+    <KPISection />
+    <Pricing />
+    <Footer />
+</template>
+
+<script lang="ts" setup>
+import Comparative from './local-components/comparative.vue';
+import KPISection from './local-components/KPISection.vue';
+</script>
---
+++ packages/ui/nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  compatibilityDate: '2024-09-23',
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+  ],
+});
