1abc6e36f03a1fa27e100b8063424e5d7edf9452
Kev1venteur <kvin.gillet@gmail.com>
2024-11-21 10:24:23 +0100

    fix: team storytelling

--- apps/web/pages/index/local-components/TeamSection.vue
+++ apps/web/pages/index/local-components/TeamSection.vue
@@ -3,7 +3,7 @@
         <div class="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
             <div class="max-w-xl bg-primary rounded-3xl p-8 xl:p-10">
                 <h2 class="text-pretty text-3xl font-semibold tracking-tight text-white sm:text-4xl">Les visages de France-Nuage</h2>
-                <p class="mt-4 font-tight text-lg/7 text-white">Fondé par une équipe d'indépendants, France-Nuage regroupe des alumnis de Système-U, Ledger, Opendatasoft, Ouest-France, Cloud-IAM et Redsmin. </p>
+                <p class="mt-4 font-tight text-lg/7 text-white">Fondé par une équipe d'indépendants, France-Nuage regroupe des alumnis fatigués du modèle de cloud traditionnel, voulant proposer une alternative où le client redeviens maitre de son destin. </p>
             </div>
             <ul role="list" class="grid sm:grid-cols-2 gap-y-12 xl:col-span-2 h-full items-center">
             <li>
@@ -11,7 +11,7 @@
                     <img class="size-20 rounded-full" :src="`/images/profil_seb.jpeg`" alt="">
                     <div>
                         <h3 class="text-base/7 font-semibold tracking-tight text-gray-900">Sébastien BERTHOME</h3>
-                        <p class="text-sm/6 font-semibold" style="color: #284ada;">Late Co-Founder / UI - UX - Product Designer</p>
+                        <p class="text-sm/6 font-semibold" style="color: #284ada;">Ex. Opendatasoft / Ledger</p>
                     </div>
                 </div>
             </li>
@@ -29,7 +29,7 @@
                     <img class="size-20 rounded-full" :src="`/images/profil_kev.jpeg`" alt="">
                     <div>
                         <h3 class="text-base/7 font-semibold tracking-tight text-gray-900">Kévin GILLET</h3>
-                        <p class="text-sm/6 font-semibold" style="color: #284ada;">Co-Founder / Responsable infrastructure et sécurité</p>
+                        <p class="text-sm/6 font-semibold" style="color: #284ada;">Ex. Système-U</p>
                     </div>
                 </div>
             </li>
@@ -38,7 +38,7 @@
                     <img class="size-20 rounded-full" :src="`/images/profil_fg.jpeg`" alt="">
                     <div>
                         <h3 class="text-base/7 font-semibold tracking-tight text-gray-900">François-Guillaume RIBREAU</h3>
-                        <p class="text-sm/6 font-semibold" style="color: #284ada;">Co-Founder / Advisor</p>
+                        <p class="text-sm/6 font-semibold" style="color: #284ada;">Ex. Ouest-France / CloudIAM / Redsmin</p>
                     </div>
                 </div>
             </li>
